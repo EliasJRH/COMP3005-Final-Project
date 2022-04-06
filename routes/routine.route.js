580c4019972bf5d567ee3538f870f67ab38806c5
Elias Hawa <elias.hawa2468@gmail.com>
2022-04-06 12:06:19 -0400

    Update routes

--- routes/routine.route.js
+++ routes/routine.route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const db = require("../database");
 
 router.get("/", async (req, res) => {
+  console.log(req.query)
   let routines = [];
   db.serialize(function () {
-    db.each("select * from routines", function (err, row) {
+    db.each("select * from routine", function (err, row) {
         routines.push(row);
     },
       function () {
@@ -14,6 +15,19 @@ router.get("/", async (req, res) => {
       }
     );
   });
+
+  // console.log(req.query)
+  // let routines = [];
+  // db.serialize(function () {
+  //   db.each("select * from routine where routine_name = '" + req.query.name + "'", function (err, row) {
+  //       routines.push(row);
+  //   },
+  //     function () {
+  //       // All done fetching records, render response
+  //       res.render("all_routines", { routines: routines });
+  //     }
+  //   );
+  // });
 });
 
 module.exports = router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,13 +4,13 @@ const db = require("../database");
 
 router.get("/", async (req, res) => {
   let users = [];
+  let count = 0;
   db.serialize(function () {
-    db.each("select * from user", function (err, row) {
-        users.push(row);
-    },
+    db.each("select * from user", function (err, row) { users.push(row);});
+    db.each("select count(*) from user", function (err, row) { count = row["count(*)"];},
       function () {
         // All done fetching records, render response
-        res.render("all_users", { users: users });
+        res.render("all_users", { users: users, count: count });
       }
     );
   });
