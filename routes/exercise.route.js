15da221192cef4fea355211554ae6fbd6fe42c9c
Elias Hawa <elias.hawa2468@gmail.com>
2022-04-08 09:52:05 -0400

    Add exercise querying

--- routes/exercise.route.js
+++ routes/exercise.route.js
@@ -3,10 +3,22 @@ const router = express.Router();
 const db = require("../database");
 
 router.get("/", async (req, res) => {
-  let exercises = [];
+  let name = req.query.name ? req.query.name : "";
+  let equipment = req.query.equipment ? req.query.equipment : "";
+  let targeted_muscle_groups = req.query.targeted_muscle_groups
+    ? req.query.targeted_muscle_groups
+    : "";
+
+  console.log(req.query);
+
   db.serialize(function () {
     db.all(
-      "select * from exercise",
+      "select * from exercise where exercise_name like $name and exercise_equipment like $equipment and targeted_muscle_groups like $tmg",
+      {
+        $name: "%" + name + "%",
+        $equipment: "%" + equipment + "%",
+        $tmg: "%" + targeted_muscle_groups + "%",
+      },
       (err, rows) => {
         // All done fetching records, render response
         res.render("all_exercises", { exercises: rows });
@@ -20,9 +32,9 @@ router.get("/:exercise_name", async (req, res) => {
   db.get(
     "select * from exercise where exercise_name = ?",
     req.params.exercise_name,
-    (err, row) =>{
+    (err, row) => {
       exercise_info = row;
-      res.render("exercise", {exercise: exercise_info})
+      res.render("exercise", { exercise: exercise_info });
     }
   );
 });
