3247b260d19c59a2cee52d5638b1be6b652a01a0
Elias Hawa <elias.hawa2468@gmail.com>
2022-04-08 10:58:10 -0400

    Finish user page

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,27 +3,61 @@ const router = express.Router();
 const db = require("../database");
 
 router.get("/", async (req, res) => {
-  let users = [];
-  let count = 0;
   db.serialize(function () {
-    db.each("select * from user", function (err, row) {
-      users.push(row);
-    });
-    db.each(
-      "select count(*) from user",
-      (err, row) => {
-        count = row["count(*)"];
-      },
-      () => {
-        // All done fetching records, render response
-        res.render("all_users", { users: users, count: count });
+    db.all(
+      "select username, email, date_of_birth from user",
+      function (err, rows) {
+        res.render("all_users", { users: rows });
       }
     );
   });
 });
 
 router.get("/:username", async (req, res) => {
-  res.render("user", { name: req.params.username });
+  friends = [];
+  made_routines = [];
+  saved_routines = [];
+  user_info = {};
+  db.serialize(() => {
+    db.all(
+      "select friends_user_two from friends where friends_user_one = ?",
+      req.params.username,
+      (err, rows) => {
+        friends = rows;
+      }
+    );
+
+    db.all(
+      "select routine_id, routine_name, routine_type from routine where made_by = ?",
+      req.params.username,
+      (err, rows) => {
+        made_routines = rows;
+      }
+    );
+
+    db.all(
+      "select routine_id, routine_name, routine_type from saved join routine on saved_routine_id = routine_id where saved_username = ?",
+      req.params.username,
+      (err, rows) => {
+        saved_routines = rows;
+      }
+    );
+
+    db.get(
+      "select username, email, date_of_birth from user where username = ?",
+      req.params.username,
+      (err, row) => {
+        user_info = row;
+        console.log(saved_routines)
+        res.render("user", {
+          user: user_info,
+          friends: friends,
+          made_routines: made_routines,
+          saved_routines: saved_routines,
+        });
+      }
+    );
+  });
 });
 
 module.exports = router;
