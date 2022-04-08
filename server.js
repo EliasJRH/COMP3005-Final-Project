bb55a60dfb8daeaa1aeb119ee786619888fa8dd6
Elias Hawa <elias.hawa2468@gmail.com>
2022-04-08 10:58:03 -0400

    Format

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 var app = express();
 
 app.get("/ping", (req, res) => {
   res.send("Pong!").status(200);
 });
 
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function (req, res, next) {
-    res.render('home');
+app.get("/", function (req, res, next) {
+  res.render("home");
 });
 
 app.use("/user", require("./routes/user.route"));
@@ -19,6 +19,9 @@ app.use("/exercise", require("./routes/exercise.route"));
 
 const PORT = process.env.PORT || 1337;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}/`)
+});
 
 module.exports = app;
